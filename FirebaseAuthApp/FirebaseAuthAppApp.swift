b90d3b39f504293a123fbf5a2ef89970fe412dec
Francis Ross Spafford <spafffr@auburn.edu>
2025-04-13 13:15:17 -0500

    Initial Commit

---
+++ FirebaseAuthApp/FirebaseAuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthAppApp.swift
+//  FirebaseAuthApp
+//
+//  Created by Ross Spafford on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
