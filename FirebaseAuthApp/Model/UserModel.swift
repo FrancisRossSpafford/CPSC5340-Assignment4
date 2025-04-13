1da6edcb12730acf3d460809d0312db6cd9cbf2d
Francis Ross Spafford <spafffr@auburn.edu>
2025-04-13 13:53:24 -0500

    Refactored to better meet MVVM pattern.  Added app title.  Other small UI changes.

---
+++ FirebaseAuthApp/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  FirebaseAuthApp
+//
+//  Created by Ross Spafford on 4/13/25.
+//
+
+import Foundation
---
+++ FirebaseAuthApp/ViewModel/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  FirebaseAuthApp
+//
+//  Created by Ross Spafford on 4/13/25.
+//
+
+import Foundation
